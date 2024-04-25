62c209814038e10ec1fa74d1ce08741a5c8e0a27
arya.bhansali <arya@minavista.org>
2024-04-25 00:31:28 -0400

    Writing out the process but we need to discuss parameters.

--- final_version.py
+++ final_version.py
@@ -1,3 +1,30 @@
 import json
 import requests
 import yfinance
+
+exchanges = ["nyse", "nasdaq", "amex"]
+
+tickers = []
+
+for stock_ex in exchanges:
+    github_branch = "https://raw.githubusercontent.com/rreichel3/US-Stock-Symbols/main"
+    exchange =  "/" + stock_ex + "/" + stock_ex + "_full_tickers.json" 
+
+    url = github_branch + exchange
+
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+
+    for i in range(len(data)):
+        ticker = data[i]
+
+        if ticker["lastsale"] == "" or ticker["volume"] == "" or ticker["marketCap"] == "":
+            continue
+
+        a = float(ticker["lastsale"][1:]) > 10 and float(ticker["volume"]) > 200000
+        b = float(ticker["marketCap"]) >= 10000000000 and float(ticker["marketCap"]) <= 15000000000
+
+        if a and b:
+            tickers.append(ticker["symbol"])
+
+print(len(tickers)) # this currently returns a list that is too large. need to probably squeeze the margin tighter
