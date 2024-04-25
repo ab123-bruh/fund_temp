98eda103e1a2c77acbf6dcafec50aeb0dbe7a105
arya.bhansali <arya@minavista.org>
2024-04-25 16:14:02 -0400

    Finding more efficient methods to parse through data.

--- final_version.py
+++ final_version.py
@@ -1,8 +1,10 @@
 import json
 import requests
-import yfinance
+import yfinance as yf
+import pandas as pd
 
-exchanges = ["nyse", "nasdaq", "amex"]
+
+exchanges = ["nyse", "nasdaq", "amex"] # US Stock Exchanges
 
 tickers = []
 
@@ -10,7 +12,7 @@ for stock_ex in exchanges:
     github_branch = "https://raw.githubusercontent.com/rreichel3/US-Stock-Symbols/main"
     exchange =  "/" + stock_ex + "/" + stock_ex + "_full_tickers.json" 
 
-    url = github_branch + exchange
+    url = github_branch + exchange # combining the repository dataset with the specific exchange
 
     resp = requests.get(url)
     data = json.loads(resp.text)
@@ -18,13 +20,28 @@ for stock_ex in exchanges:
     for i in range(len(data)):
         ticker = data[i]
 
+        # important to note that since the list of tickers is massive, need to find any method to narrow the scope
+
+        # Step 1: Check to see if anything is blank from the full json file
         if ticker["lastsale"] == "" or ticker["volume"] == "" or ticker["marketCap"] == "":
             continue
-
+        
+        # Step 2: eliminate low cost stocks, less volume, and bound the marketcap due to portfolio size
         a = float(ticker["lastsale"][1:]) > 10 and float(ticker["volume"]) > 200000
-        b = float(ticker["marketCap"]) >= 10000000000 and float(ticker["marketCap"]) <= 15000000000
+        b = float(ticker["marketCap"]) >= 2000000000 and float(ticker["marketCap"]) <= 15000000000
 
         if a and b:
-            tickers.append(ticker["symbol"])
+            # Step 3: gather data of specific metrics which we will use to eliminate any that don't fit in criteria
+            #         and if any of these are missing, they are automatically eliminated from our scope 
+            metric_evaluation = ["symbol", "beta", "revenueGrowth", "priceToBook", "debtToEquity", 
+                                 "profitMargins", "quickRatio", "fiftyDayAverage", "pegRatio"]
+            
+            try:
+                tick = pd.DataFrame(yf.Ticker(ticker["symbol"]).info)[metric_evaluation].drop_duplicates()
+                tickers.append(tick)
+            except:
+                continue
+    
+tickers = pd.concat(tickers,axis=0).reset_index(drop=True)
 
-print(len(tickers)) # this currently returns a list that is too large. need to probably squeeze the margin tighter
+print(tickers) # this currently takes too long to process. need to make it shorter
