a6be900b432de400b2efbaf3b25944e6f682a0ca
arya.bhansali <arya@minavista.org>
2024-04-26 00:03:02 -0400

    AC PLEASE EDIT.

--- final_version.py
+++ final_version.py
@@ -33,39 +33,35 @@ for stock_ex in exchanges:
         ticker["marketCap"] = float(ticker["marketCap"])
 
         # Step 2: eliminate low cost stocks, less volume, and bound the marketcap due to portfolio size
-        check_1 = ticker["lastsale"] > 10 and ticker["volume"] > 200000
+        check_1 = ticker["lastsale"] > 5 and ticker["volume"] > 200000
         check_2 = ticker["marketCap"] >= 2000000000 and ticker["marketCap"] <= 15000000000
 
         if check_1 and check_2:
             values = dict(filter(lambda item: item[0] in metric_evaluation, 
                                  yf.Ticker(ticker[metric_evaluation[0]]).info.items()))
             
-
-            check_3 = ticker["lastsale"] > values[metric_evaluation[8]]
-            
-            if check_3: 
-                continue
-
             tickers.append(values)
             
-
 # Putting the symbol first for readability purposes
 tickers = pd.DataFrame(tickers)
 
 tickers = tickers.dropna(axis=0)
 
+# Convert to numeric to eliminate based on certain conditions
 for col in metric_evaluation[1:]:
     tickers[col] = pd.to_numeric(tickers[col])
+    
 
 # Step 3: eliminate any criteria which is greater or less than right away so we have a smaller dataset 
 #         which this was started in the first check of seeing if latest price less than 50 day moving average
 tickers = tickers.loc[(tickers[metric_evaluation[3]] > .05) & (tickers[metric_evaluation[4]] < 12) 
                       & (tickers[metric_evaluation[5]] < 100) & (tickers[metric_evaluation[6]] > .15) 
-                      & (tickers[metric_evaluation[7]] > 1) & (tickers[metric_evaluation[9]] < 5)]
+                      & (tickers[metric_evaluation[7]] > 1) & (tickers[metric_evaluation[8]] > tickers[metric_evaluation[1]])
+                      & (tickers[metric_evaluation[9]] < 5)]
 
-
+# Step 4: out of the eliminated criteria from previous, take beta between .5 & 1.5
 tickers = tickers.loc[(tickers[metric_evaluation[2]] >= .5) & (tickers[metric_evaluation[2]] <= 1.5)]
 
 tickers = tickers[metric_evaluation].reset_index(drop=True)
 
-print(tickers) # this currently takes too long to process. need to make it shorter
+print(tickers)
