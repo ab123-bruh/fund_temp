073468b81d870d9e8278a392d0dccd7d2826d0e1
arya.bhansali <arya@minavista.org>
2024-04-28 14:27:15 -0400

    Refer to criteria JSON when reading the code.

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,14 +3,12 @@ import requests
 import yfinance as yf
 import pandas as pd
 
-exchanges = ["nyse", "nasdaq", "amex"] # US Stock Exchanges
-
-metric_evaluation = ["symbol", "currentPrice", "beta", "revenueGrowth", "priceToBook", "debtToEquity", "profitMargins", 
-                     "quickRatio", "fiftyDayAverage", "pegRatio"]
+# Click on the file to refer to the criteria set or print it in the test jupyter notebook
+criteria = json.loads(open("criteria.json","r").read())
 
 tickers = []
 
-for stock_ex in exchanges:
+for stock_ex in criteria["Exchanges"]:
     github_branch = "https://raw.githubusercontent.com/rreichel3/US-Stock-Symbols/main"
     exchange =  "/" + stock_ex + "/" + stock_ex + "_full_tickers.json" 
 
@@ -22,23 +20,27 @@ for stock_ex in exchanges:
     for i in range(len(data)):
         ticker = data[i]
 
+        immediate_criteria = list(criteria["Immediate Criteria"].keys()) # ["lastsale", "volume", "marketCap"]
+        
         # important to note that since the list of tickers is massive, need to find any method to narrow scope
 
-        # Step 1: Check to see if anything is blank from the full json file
-        if ticker["lastsale"] == "" or ticker["volume"] == "" or ticker["marketCap"] == "":
+        # Step 1: Check to see if anything is blank from the full json file or ticker already in portfolio
+        if ticker[criteria["Metrics"][0]] in list(criteria["Portfolio Weights"].keys()) or any(ticker[checker] == "" for checker in immediate_criteria): 
             continue
 
-        ticker["lastsale"] = float(ticker["lastsale"][1:])
-        ticker["volume"] = float(ticker["volume"])
-        ticker["marketCap"] = float(ticker["marketCap"])
+        ticker[immediate_criteria[0]] = float(ticker[immediate_criteria[0]][1:])
+        ticker[immediate_criteria[1]] = float(ticker[immediate_criteria[1]])
+        ticker[immediate_criteria[2]] = float(ticker[immediate_criteria[2]])
 
         # Step 2: eliminate low cost stocks, less volume, and bound the marketcap due to portfolio size
-        check_1 = ticker["lastsale"] > 5 and ticker["volume"] > 200000
-        check_2 = ticker["marketCap"] >= 2000000000 and ticker["marketCap"] <= 15000000000
+        check_1 = ticker[immediate_criteria[0]] > criteria["Immediate Criteria"][immediate_criteria[0]] \
+            and ticker[immediate_criteria[1]] > criteria["Immediate Criteria"][immediate_criteria[1]]
+        check_2 = ticker[immediate_criteria[2]] >= criteria["Immediate Criteria"][immediate_criteria[2]]["min"] \
+                         and ticker[immediate_criteria[2]] <= criteria["Immediate Criteria"][immediate_criteria[2]]["max"]
 
         if check_1 and check_2:
-            values = dict(filter(lambda item: item[0] in metric_evaluation, 
-                                 yf.Ticker(ticker[metric_evaluation[0]]).info.items()))
+            values = dict(filter(lambda item: item[0] in criteria["Metrics"], 
+                                 yf.Ticker(ticker[criteria["Metrics"][0]]).info.items()))
             
             tickers.append(values)
             
@@ -48,20 +50,28 @@ tickers = pd.DataFrame(tickers)
 tickers = tickers.dropna(axis=0)
 
 # Convert to numeric to eliminate based on certain conditions
-for col in metric_evaluation[1:]:
+for col in criteria["Metrics"][1:]:
     tickers[col] = pd.to_numeric(tickers[col])
     
+# Step 3: eliminate any criteria which we have greater than
+portfolio_criteria = list(criteria["Portfolio Criteria"]["Greater"].keys()) 
+for column in portfolio_criteria:
+    tickers = tickers.loc[tickers[column] > criteria["Portfolio Criteria"]["Greater"][column]]
 
-# Step 3: eliminate any criteria which is greater or less than right away so we have a smaller dataset 
-#         which this was started in the first check of seeing if latest price less than 50 day moving average
-tickers = tickers.loc[(tickers[metric_evaluation[3]] > .05) & (tickers[metric_evaluation[4]] < 12) 
-                      & (tickers[metric_evaluation[5]] < 100) & (tickers[metric_evaluation[6]] > .15) 
-                      & (tickers[metric_evaluation[7]] > 1) & (tickers[metric_evaluation[8]] > tickers[metric_evaluation[1]])
-                      & (tickers[metric_evaluation[9]] < 5)]
+# Step 4: eliminate any criteria which we have less than
+portfolio_criteria = list(criteria["Portfolio Criteria"]["Less Than"].keys()) 
+for column in portfolio_criteria:
+    tickers = tickers.loc[tickers[column] < criteria["Portfolio Criteria"]["Less Than"][column]]
 
-# Step 4: out of the eliminated criteria from previous, take beta between .5 & 1.5
-tickers = tickers.loc[(tickers[metric_evaluation[2]] >= .5) & (tickers[metric_evaluation[2]] <= 1.5)]
 
-tickers = tickers[metric_evaluation].reset_index(drop=True)
+# Step 5: range of criteria
+beta = criteria["Metrics"][2]
+beta_min = criteria["Portfolio Criteria"]["beta"]["min"]
+beta_max = criteria["Portfolio Criteria"]["beta"]["max"]
 
-print(tickers["symbol"].tolist())
+tickers = tickers.loc[(tickers[beta] >= beta_min) & (tickers[beta] <= beta_max) 
+                      & (tickers[criteria["Metrics"][8]] > tickers[criteria["Metrics"][1]])]
+
+tickers = tickers[criteria["Metrics"]].reset_index(drop=True)
+
+print(tickers[criteria["Metrics"][0]].tolist())
