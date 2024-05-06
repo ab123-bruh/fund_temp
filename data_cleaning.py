a4e09b027fc1f370f01c3d86c1e2e26a7962d531
arya.bhansali <arya@minavista.org>
2024-05-06 15:24:19 -0400

    Starting to boot up backtesting environment.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,9 +1,10 @@
 import json
 import requests
+import os
 import yfinance as yf
 
 # Click on the file to refer to the criteria set or print it in the test jupyter notebook
-criteria = json.loads(open("criteria.json","r").read())
+criteria = json.loads(open(os.getcwd() + "\\criteria.json","r").read())
 
 tickers = []
 
@@ -20,14 +21,14 @@ for stock_ex in criteria["Exchanges"]:
         ticker = data[i]
 
         immediate_criteria = list(criteria["Immediate Criteria"].keys()) # ["lastsale", "volume", "marketCap"]
-        symbol = criteria["Metrics"][0]
         
         # important to note that since the list of tickers is massive, need to find any method to narrow scope
 
         # Step 1: Check to see if anything is blank from the full json file or ticker already in portfolio
-        if ticker[symbol] in list(criteria["Portfolio Weights"].keys()) \
+        if ticker["symbol"] in list(criteria["Portfolio Weights"].keys()) \
               or any(ticker[checker] == "" for checker in immediate_criteria): continue
 
+        # numeric comparison
         lastsale = float(ticker[immediate_criteria[0]][1:])
         volume = float(ticker[immediate_criteria[1]])
         marketCap = float(ticker[immediate_criteria[2]])
@@ -40,22 +41,26 @@ for stock_ex in criteria["Exchanges"]:
                          and marketCap <= criteria["Immediate Criteria"][immediate_criteria[2]]["max"]
 
         if check_1 and check_2:
-            values = dict(filter(lambda item: item[0] in criteria["Metrics"], 
-                                 yf.Ticker(ticker[symbol]).info.items()))
-            
             greater = criteria["Portfolio Criteria"]["Greater"]
             less_than = criteria["Portfolio Criteria"]["Less Than"]
 
-            try:
-                # Step 3: select tickers whick satisfy all of these conditions
-                check_3 = all(values[metric] > x for metric,x in greater.items()) and all(values[metric] < x for metric,x in less_than.items())
-                check_4 = values[criteria["Metrics"][8]] < values[criteria["Metrics"][1]]
+            symbol = ticker["symbol"]
 
-                if check_3 and check_4:
-                    tickers.append(values[symbol])
+            # list of metrics for criteria
+            metrics = list(greater.keys())
+            metrics.extend(list(less_than.keys()))
 
-            except KeyError:
-                continue
+#             values = dict(filter(lambda item: item[0] in metrics, yf.Ticker(symbol).info.items()))
             
-print(tickers)
+#             try:
+#                 # Step 3: select tickers whick satisfy all of these conditions
+#                 check_3 = all(values[metric] > x for metric,x in greater.items()) 
+#                 check_4 = all(values[metric] < x for metric,x in less_than.items())
+#                 if check_3 and check_4:
+#                     tickers.append(symbol)
+
+#             except KeyError:
+#                 continue
+            
+# print(tickers)
 
--- portfolio_allocation.py
+++ portfolio_allocation.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+import bt
+from datetime import datetime
+from datetime import timedelta
 
 tickers = subprocess.run(args=["python", os.getcwd() + "\\data_cleaning.py"],text=True,stdout=subprocess.PIPE)
 
@@ -7,4 +10,6 @@ results = tickers.stdout.strip('""[]\n').split(', ')
 
 symbols = [element.strip("'\"") for element in results]
 
-print(symbols)
+start = (datetime.now() - timedelta(days=365)).strftime("%Y-%m-%d")
+
+print(bt.get(symbols,start=start))
