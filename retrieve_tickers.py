d80296b6d6b4ca3e5bb9ff675843e20e91fa8e15
arya.bhansali <arya@minavista.org>
2024-05-22 22:21:33 -0400

    Run time to collect tickers to be tested now.

--- retrieve_tickers.py
+++ retrieve_tickers.py
@@ -2,6 +2,7 @@ import json
 import os
 import requests
 import yfinance as yf
+import numpy as np
 
 criteria = json.loads(open(os.getcwd() + "\\criteria.json","r").read())
   
@@ -31,54 +32,68 @@ class Ticker:
     def __init__(self):
         self.github_branch = "https://raw.githubusercontent.com/rreichel3/US-Stock-Symbols/main"
         self.exchanges = criteria["Exchanges"]
+        self.file =  "_full_tickers.json"
     
     def get_all_tickers(self):
         tickers = {}
 
         for stock_ex in self.exchanges:
-            exchange =  "/" + stock_ex + "/" + stock_ex + "_tickers.txt"
+            exchange =  "/" + stock_ex + "/" + stock_ex + self.file
             resp = requests.get(self.github_branch + exchange)
-            data = resp.text.split("\n")
-            tickers[stock_ex] = data
+            tickers[stock_ex] = json.loads(resp.text)
         
         return tickers
     
     def shortlist_tickers(self):
+        tickers = Ticker().get_all_tickers()
         new_tickers = []
 
+        crit = list(criteria["Immediate Criteria"].keys())
+
         for stock_ex in self.exchanges:
-            exchange =  "/" + stock_ex + "/" + stock_ex + "_full_tickers.json" 
+            data = tickers[stock_ex]
 
-            url = self.github_branch + exchange # combining the repository dataset with the specific exchange
+            ticker_data = []
+            metric_data = {metric: [] for metric in crit}
 
-            resp = requests.get(url)
-            data = json.loads(resp.text)
+            for tick_val in data:
+                check_1 = tick_val["symbol"] in list(Basket().get_portfolio().keys())
+                check_2 = any(tick_val[checker] == "" for checker in crit)
 
-            for i in range(len(data)):
-                ticker = data[i]
-
-                check_1 = ticker["symbol"] in list(Basket().get_portfolio().keys())
-                check_2 = any(ticker[checker] == "" for checker in list(criteria["Immediate Criteria"].keys()))
-                
-                # important to note that since the list of tickers is massive, need to find any method to narrow scope
-
-                if check_1 or check_2: 
+                if check_1 or check_2:
                     continue
+                
+                lastsale = float(tick_val[crit[0]][1:])
+                volume = float(tick_val[crit[1]])
+                marketCap = float(tick_val[crit[2]])
 
-                # numeric comparison
-                lastsale = float(ticker["lastsale"][1:])
-                volume = float(ticker["volume"])
-                marketCap = float(ticker["marketCap"])
-
-                check_3 = lastsale > criteria["Immediate Criteria"]["lastsale"] and \
-                    volume > criteria["Immediate Criteria"]["volume"]
-                    
-                check_4 = marketCap <= criteria["Immediate Criteria"]["marketCap"]
-
-                if check_3 and check_4:
-                     new_tickers.append(ticker["symbol"])
+                ticker_data.append(tick_val["symbol"])
+                
+                # loop unrolling to save time here
+                metric_data[crit[0]].append(lastsale)
+                metric_data[crit[1]].append(volume)
+                metric_data[crit[2]].append(marketCap)
+                        
+            metric_arrays = {metric: np.array(values) for metric, values in metric_data.items()}
             
+            check_3 = np.ones(len(ticker_data), dtype=bool)
+            check_4 = np.ones(len(ticker_data), dtype=bool)
+
+            check_3 &= metric_arrays[crit[0]] > criteria["Immediate Criteria"][crit[0]]
+            check_3 &= metric_arrays[crit[1]] > criteria["Immediate Criteria"][crit[1]]
+
+            check_4 &= metric_arrays[crit[2]] < criteria["Immediate Criteria"][crit[2]]
+
+            final_check = check_3 & check_4
+
+            ticker_data = np.array(ticker_data)[final_check].tolist()
+
+            new_tickers.extend(ticker_data)
+        
+        new_tickers = [tick.replace("/",".") for tick in new_tickers if "^" not in tick]
+
         return new_tickers
+
 
     def recommend_tickers(self):
         tickers = Ticker().shortlist_tickers()
@@ -118,3 +133,6 @@ class Ticker:
 #         self.symbols.extend(DataClean().add_multiple_tickers())
 
 #         return yf.download(tickers=self.symbols,period=period)
+
+
+print(Ticker().recommend_tickers())
